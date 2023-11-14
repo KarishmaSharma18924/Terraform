1f85b85322ee6c5bffc2c48c00f2cc41b28abb9a
KarishmaSharma18924 <150773354+KarishmaSharma18924@users.noreply.github.com>
2023-11-14 11:41:51 +0530

    Created main.tf

---
+++ main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+
+  region = "us-east-1"
+
+}
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+
+ 
+
+  filter {
+
+    name   = "name"
+
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+
+  }
+
+ 
+
+  filter {
+
+    name   = "virtualization-type"
+
+    values = ["hvm"]
+
+  }
+
+ 
+
+  owners = ["099720109477"] # Canonical
+
+}
+esource "aws_instance" "instance" {
+
+  ami           = data.aws_ami.ubuntu.id
+
+  instance_type = "t3.micro"
+
+  count=4
+
+  key_name="kiddcorp"
+
+  tags = {
+
+    Name = "instance-${count.index}",
+
+    role=count.index==0?"lb": (count.index<3?"web":"backend")
+
+  }
+
+}
+
+ 
+
+output "ips"{
+
+  value = aws_instance.instance.*.public_ip
+
+}
